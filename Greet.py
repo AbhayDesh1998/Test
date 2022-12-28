f23e60248509620f0f0da72ab8b91aa52d6c513e
unknown <a8867731719@gmail.com>
2022-12-28 20:03:50 +0530

    Commiting changes for Greet.py and test.py

--- Greet.py
+++ Greet.py
@@ -2,6 +2,7 @@ greet = input("Hello, How Are you? : ")
 
 if greet == 'fine':
     print("Iam Good")
+    print("Want Anything??")
 
 else:
-    print("Iam not well):");
+    print("Iam not well):")
--- test.py
+++ test.py
@@ -1,1 +1,2 @@
 print("This file is for results of testing")
+print("This file is also dedicated for storing output")
