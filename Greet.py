ae50db8b1c0ab761fa092f091d6f3d7d5241b82a
unknown <a8867731719@gmail.com>
2022-12-28 20:13:43 +0530

    Commiting Greet.py first version to feature branch

--- Greet.py
+++ Greet.py
@@ -2,7 +2,12 @@ greet = input("Hello, How Are you? : ")
 
 if greet == 'fine':
     print("Iam Good")
-    print("Want Anything??")
+    req = input("Want Anything??")
+
+    if req == 'food':
+        print("Have Food!")
+    else:
+        print("Get Relax!")
 
 else:
     print("Iam not well):")
