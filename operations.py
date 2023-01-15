4e87eb0f942c45523ffa986a68556a8ae42fd66c
unknown <a8867731719@gmail.com>
2023-01-15 15:52:12 +0530

    Final Commit

---
+++ operations.py
@@ -0,0 +1,21 @@
+
+
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
+
+print(add(a,b))
+print(mul(a,b))
+print(square(a))
+
+def add(a,b):
+    return a+b
+
+def mul(a,b):
+    return a*b
+
+def square(a):
+    return a*a
+
+
+
+
